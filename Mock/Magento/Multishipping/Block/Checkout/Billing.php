8467db76335f2bf36b08404c0eddfb0947876cff
CWCS Dev <cwcs.dev@fashionwallpaper.co.uk>
2025-06-03 16:29:12 +0100

    1.0.3

---
+++ Mock/Magento/Multishipping/Block/Checkout/Billing.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright 2011 Adobe
+ * All Rights Reserved.
+ */
+namespace Magento\Multishipping\Block\Checkout;
+
+class Billing extends \Magento\Payment\Block\Form\Container
+{
+}
---
+++ Mock/Magento/Multishipping/Block/Checkout/Success.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright 2011 Adobe
+ * All Rights Reserved.
+ */
+namespace Magento\Multishipping\Block\Checkout;
+
+class Success extends \Magento\Payment\Block\Form\Container
+{
+}
