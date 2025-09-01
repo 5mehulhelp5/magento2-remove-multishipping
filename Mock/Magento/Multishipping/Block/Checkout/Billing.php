245e06668f5dc856593e78ca1b12441735afd1f7
CWCS Dev <cwcs.dev@fashionwallpaper.co.uk>
2025-09-01 14:55:11 +0100

    1.0.5

--- Mock/Magento/Multishipping/Block/Checkout/Billing.php
+++ Mock/Magento/Multishipping/Block/Checkout/Billing.php
@@ -7,4 +7,17 @@ namespace Magento\Multishipping\Block\Checkout;
 
 class Billing extends \Magento\Payment\Block\Form\Container
 {
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Payment\Helper\Data $paymentHelper,
+        \Magento\Payment\Model\Checks\SpecificationFactory $methodSpecificationFactory,
+        \Magento\Multishipping\Model\Checkout\Type\Multishipping $multishipping,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Payment\Model\Method\SpecificationInterface $paymentSpecification,
+        array $data = [],
+        array $additionalChecks = []
+    ) {
+        parent::__construct($context, $paymentHelper, $methodSpecificationFactory, $data, $additionalChecks);
+        $this->_isScopePrivate = true;
+    }
 }
